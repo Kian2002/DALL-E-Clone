1bdca1973146874b3b04644242b579704fafb2e3
Kian2002 <canadakurdistan@gmail.com>
2023-03-18 02:20:26 -0700

    create 'createPost' page & search bar

--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -1,7 +1,123 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+
+  const [generatingImage, setGeneratingImage] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+    // TODO: Handle form submission after making backend
+  };
+
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+
+    setForm((prev) => ({ ...prev, prompt: randomPrompt }));
+  };
+
+  const handleGenerateImage = () => {
+    // TODO: Handle image generation after making backend
+  };
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="text-[#222328] font-extrabold text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+          Generate an imaginative image through the DALL-E AI and share it with
+          the community
+        </p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            value={form.name}
+            placeholder="John Doe"
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            value={form.prompt}
+            placeholder="A monkey jumping on a bed"
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+
+          <div
+            className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+        focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center"
+          >
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="Preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImage && (
+              <div className="absolute bg-[rgba(0,0,0,0.5)] flex justify-center items-center rounded-lg inset-0 z-0">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={handleGenerateImage}
+            className="bg-green-700 text-gray-300 px-5 py-2.5 text-center rounded-md font-medium text-sm w-full sm:w-auto"
+          >
+            {generatingImage ? "Generating..." : "Generate"}
+          </button>
+        </div>
+
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            ** Once you have created the image you want, you can share it with
+            others in the community **
+          </p>
+          <button
+            type="submit"
+            className=" bg-[#4649ff] text-gray-300 px-5 py-2.5 text-center rounded-md font-medium text-sm w-full sm:w-auto mt-3"
+          >
+            {loading ? "Sharing..." : "Share with the community"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePost;
