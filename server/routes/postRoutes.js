6c10f59b9ef4466fd0523279a6323d413b983a9d
Kian2002 <canadakurdistan@gmail.com>
2023-03-18 16:34:23 -0700

    create image generation route

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,13 +1,17 @@
 import express from "express";
 import { v2 as cloudinary } from "cloudinary";
-import { Router } from "express";
 
 import Post from "../mongodb/models/post.js";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.send("Hello From DALL-E Backend!");
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 });
 
 export default router;
